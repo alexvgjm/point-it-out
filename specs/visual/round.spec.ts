fea142bb3cb55ed3b2c2240bdbf259f319694e72
Alex Vega <alxvj2@gmail.com>
2024-07-23 03:56:53 +0100

    🔵🟢 Round SVG follows target

--- specs/visual/round.spec.ts
+++ specs/visual/round.spec.ts
@@ -8,6 +8,34 @@ test.describe("create('round')", () => {
         { expectW: 400, expectH: 250 }
     ]
 
+    testsTargets.forEach(({ expectW, expectH }, idx) => {
+        test(`created SVG follow target element on resize (${expectW}x${expectH})`, async ({page}, testInfo)=>{
+            await visualComparisonBetweenPages({
+                testingURL: `/${expectW}x${expectH}`,
+                expectedURL: `/expected/round/${expectW}x${expectH}-default`,
+
+                beforeExpectedScreenshot: 
+                    async () => await page.setViewportSize({width: 600, height: 600}),
+
+                beforeAction: 
+                    async () => await page.setViewportSize({width: 1280, height: 768}),
+
+                action: async () => {
+                    await page.evaluate(({ expectW, expectH }) => pio.create('round', {
+                        target: `.test-box--${expectW}x${expectH}`,
+                        className: `result`,
+                    }), { expectW, expectH })
+                },
+
+                beforeActionScreenshot: 
+                    async () => await page.setViewportSize({width: 600, height: 600}),
+
+                pwPage: page,
+                pwTestInfo: testInfo,
+            })
+        })
+    })
+
     test.describe('Default behavior (no options)', () => {
         testsTargets.forEach(({ expectW, expectH }, idx) => {
             it(`creates an orange ellipse with a width of 4px around target (${expectW}x${expectH})`, async ({ page }, testInfo) => {
