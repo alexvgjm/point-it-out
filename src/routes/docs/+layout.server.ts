50bc32013fab9111bfcde5bb9f208c1650a4dfe8
Alexander Vega <alxvj2@gmail.com>
2024-08-08 20:23:31 +0100

    📚❌ Removed docs (migrated to another repo)

--- src/routes/docs/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import type { LayoutServerLoad } from './$types'
-
-export const prerender = true
-
-export const load = (async () => {
-  return {}
-}) satisfies LayoutServerLoad
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { readFileSync, writeFileSync } from 'fs'
-import { fileURLToPath } from 'url'
 import { defineConfig } from 'vitest/config'
-
-const file = fileURLToPath(new URL('package.json', import.meta.url))
-const json = readFileSync(file, 'utf8')
-const pkg = JSON.parse(json)
-
-/**
- * FIXME: static adapter requires $env/static and not $env/dynamic. Need to
- * investigate how to add version programmatically without .env file.
- */
-process.env.PUBLIC_VERSION = pkg.version
-writeFileSync(
-  './.env',
-  `
-PUBLIC_VERSION=${pkg.version}
-PUBLIC_DOCS_ROOT=/docs
-`
-)
 
 export default defineConfig({
   plugins: [sveltekit()],
