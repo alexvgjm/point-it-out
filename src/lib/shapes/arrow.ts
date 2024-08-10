c87c3b88e440b56233f6c0483df253b1a863c004
Alexander Vega <alxvj2@gmail.com>
2024-08-10 02:08:07 +0100

    Recovering stash after merge

--- src/lib/shapes/arrow.ts
+++ src/lib/shapes/arrow.ts
@@ -1,6 +1,20 @@
 import { createSVG, PointItOutPointer } from './core'
 import type { PointerOptions } from '../types'
 
+function createArrowDPathAttribute(w: number, len: number) {
+  // pretier-ignore
+  const atr = `M 0,          ${w / 2}
+   L ${len / 2},   ${w}
+   l 0,         -${w / 3}
+   l ${len / 2},   0
+   l 0,         -${w / 3}
+   l -${len / 2},  0
+   l 0,         -${w / 3}
+   Z`
+
+  return atr
+}
+
 export class ArrowPointer extends PointItOutPointer {
   path: SVGPathElement
 
@@ -15,6 +29,6 @@ export class ArrowPointer extends PointItOutPointer {
   }
 
   update(): void {
-    // TODO:
+    this.path.setAttribute('d', createArrowDPathAttribute(128, 96))
   }
 }
