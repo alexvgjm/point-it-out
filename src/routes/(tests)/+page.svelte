52116f32dbcb003f5eff721623a13ffd7e48faf3
Alexander Vega <alxvj2@gmail.com>
2024-08-10 16:11:20 +0100

    🩹 Fix tests index

--- src/routes/(tests)/+page.svelte
+++ src/routes/(tests)/+page.svelte
@@ -10,33 +10,55 @@
     <h3>create('rect', {'{...}'})</h3>
     <ul>
       <li>
-        <a href="/rect/300x300/default"><span class="size">300x300</span></a>
-        <a href="/rect/400x250/default"><span class="size">400x250</span></a>
+        <a href="/300x300/rect/default"><span class="size">300x300</span></a>
+        <a href="/400x250/rect/default"><span class="size">400x250</span></a>
         defaults
       </li>
 
       <li>
-        <a href="/rect/300x300/padding-option"><span class="size">300x300</span></a>
-        <a href="/rect/400x250/padding-option"><span class="size">400x250</span></a>
+        <a href="/300x300/rect/padding-option"><span class="size">300x300</span></a>
+        <a href="/400x250/rect/padding-option"><span class="size">400x250</span></a>
         padding option
       </li>
 
       <li>
-        <a href="/rect/300x300/stroke-width-color-options"><span class="size">300x300</span></a>
-        <a href="/rect/400x250/stroke-width-color-options"><span class="size">400x250</span></a>
+        <a href="/300x300/rect/stroke-width-color-options"><span class="size">300x300</span></a>
+        <a href="/400x250/rect/stroke-width-color-options"><span class="size">400x250</span></a>
         strokeWidth & color options
       </li>
 
       <li>
-        <a href="/rect/300x300/round-option"><span class="size">300x300</span></a>
-        <a href="/rect/400x250/round-option"><span class="size">400x250</span></a>
-        round option
+        <a href="/300x300/rect/round-option/pixels"><span class="size">300x300</span></a>
+        <a href="/400x250/rect/round-option/pixels"><span class="size">400x250</span></a>
+        round option (pixels)
+      </li>
+      <li>
+        <a href="/300x300/rect/round-option/percent"><span class="size">300x300</span></a>
+        <a href="/400x250/rect/round-option/percent"><span class="size">400x250</span></a>
+        round option (percent)
+      </li>
+      <li>
+        <a href="/300x300/rect/round-option/x-and-y"><span class="size">300x300</span></a>
+        <a href="/400x250/rect/round-option/x-and-y"><span class="size">400x250</span></a>
+        round option (different axis)
       </li>
     </ul>
   </div>
 
   <div class="category">
-    <h3>create('image', {'{...}'})</h3>
-    Coming soon...
+    <h3>create('arrow', {'{...}'})</h3>
+    <ul>
+      <li>
+        <a href="/300x300/arrow/default"><span class="size">300x300</span></a>
+        defaults
+      </li>
+      <li>
+        <a href="/300x300/arrow/from-option/120"><span class="size">300x300</span></a>
+        <a href="/300x300/arrow/from-option/90"><span class="size">90º</span></a>
+        <a href="/300x300/arrow/from-option/180"><span class="size">180º</span></a>
+        <a href="/300x300/arrow/from-option/328"><span class="size">328º</span></a>
+        fromAngle
+      </li>
+    </ul>
   </div>
 </section>
