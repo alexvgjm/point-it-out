3d41f121ec4f468ffd69d47a92655efde9ec4897
Alexander Vega <alxvj2@gmail.com>
2025-05-01 15:23:53 +0100

    🧪📦 Version 0.2.2

---
+++ src/routes/[size]/static-positioned/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { PageData } from './$types'
+
+  export let data: PageData
+</script>
+
+<section>
+  <div
+    class="test-box test-box--{data.w}x{data.h}"
+    style="width: {data.w}px; height: {data.h}px"
+  ></div>
+</section>
+
+<style>
+  .test-box {
+    position: static;
+    margin-left: 30px;
+  }
+
+  section {
+    margin-right: 12px;
+  }
+
+  :global(body.visual-test) {
+    display: block;
+  }
+</style>
